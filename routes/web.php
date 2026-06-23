3f5a99f1a9b6c2a65af872520f093c49c735741c
Gracy Abinaya <gracyabinaya219.com>
2026-06-23 14:49:29 +0530

    resolved merge conflict in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,24 @@
 
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\DepartmentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+// Courses
+Route::resource('courses', CourseController::class);
 
+// Departments
+Route::resource('departments', DepartmentController::class);
+
+// Students (resource without destroy)
 Route::resource('students', StudentController::class)->except(['destroy']);
-Route::delete('students/{student}', [StudentController::class, 'destroy'])->name('students.destroy');
-Route::get('students/{student}/print', [StudentController::class, 'print'])->name('students.print');
+
+// Custom student routes
+Route::delete('students/{student}', [StudentController::class, 'destroy'])
+    ->name('students.destroy');
+
+Route::get('students/{student}/print', [StudentController::class, 'print'])
+    ->name('students.print');
